9341a3043650d69e437511889f97ebf8fe85c95c
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-01-11 09:20:09 -0300

    first commit

---
+++ src/test/java/com/api/equipment/EquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
