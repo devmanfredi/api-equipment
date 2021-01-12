e16c0b61daa5162fec0e5acab202ca41e6c51ff3
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2021-01-12 14:54:58 -0300

    classe de persistencia

---
+++ src/main/java/com/api/equipment/repository/EquipamentoRepository.java
@@ -0,0 +1,10 @@
+package com.api.equipment.repository;
+
+import com.api.equipment.model.Equipamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipamentoRepository extends JpaRepository<Equipamento, Long> {
+
+}
